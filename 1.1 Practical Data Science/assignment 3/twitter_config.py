0f3ecc23c26a56cb635dc10f96d5a6869d33a499
SofiaMpaltzi <sofiampaltzi@gmail.com>
2020-12-20 21:54:46 +0200

    Add files via upload

---
+++ 1.1 Practical Data Science/assignment 3/twitter_config.py
@@ -0,0 +1,7 @@
+config = {
+    'consumer_key': 'ZKVrwn5h71TxpAsL6x55BIR8m',
+    'consumer_secret': 'vEyFUptKTo5McmlnDQ9ohuTJ058cjmbIqpIBqSfUGmlvrVQCh9',
+
+    'access_token': '1137364419686608896-HMr8XJL6vqOWMPCNOYsbg6FXfCHC4c',
+    'access_token_secret': 'Zz2refjpXDuwr2St9BYSw5gz9Hqf1JdgWF7BgeI9ChN2g'
+}
